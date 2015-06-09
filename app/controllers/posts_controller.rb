7c03dc115e2cbe1ac7af7cce06d42d6af189e952
s-nakayama0726 <s-nakayama@ankh-systems.co.jp>
2015-06-09 11:36:38 +0900

    投稿画面テスト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,18 @@ class PostsController < ApplicationController
 
 #POST /posts
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new(post_params)
     @post.save
     redirect_to @post
   end
   
+  #GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   private
   def post_params
-     params.require(:post).permit(:title, :text)
+     params.require(:post).permit(:title,:text)
   end
 end
